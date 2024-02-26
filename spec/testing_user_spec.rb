5d0552bdc309c8c0c1e780f1dbd833ba64285700
Leah Bennie <lcbennie05@gmail.com>
2024-02-26 15:26:29 -0600

    completed testing user problem

---
+++ spec/testing_user_spec.rb
@@ -0,0 +1,28 @@
+require '../testing_user'
+
+RSpec.describe User do
+  describe "#log_in" do
+    it "logs the user in" do
+      user = User.new('garfield')
+      user.log_in
+      expect(user.logged_in).to eq(true)
+    end
+  end
+
+  describe "#log_out" do
+    it "logs the user out" do
+      user = User.new('garfield')
+      user.log_in
+      user.log_out
+      expect(user.logged_in).to eq(false)
+    end
+  end
+
+  describe "#username" do
+    it "returns the correct username" do
+      username = 'garfield'
+      user = User.new(username)
+      expect(user.username).to eq(username)
+    end
+  end
+end
